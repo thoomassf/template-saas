8581f26c517673f00cac0e7f02c2cd9b161d26c6
Thomas Farias <thomas.farias@sempreit.com.br>
2025-04-17 20:46:14 -0300

    lib firebase

---
+++ app/lib/firebase.ts
@@ -0,0 +1,26 @@
+import "server-only"
+import { cert, getApps, initializeApp } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+// import { getStorage } from "firebase-admin/storage"
+
+if (!process.env.FIREBASE_PRODUCT_ID) {
+  throw new Error("FIREBASE_PRODUCT_ID is not set")
+}
+
+const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64!, "base64").toString("utf-8")
+
+export const firebaseCert = cert({
+  projectId: process.env.FIREBASE_PRODUCT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: decodedKey,
+})
+
+if (!getApps().length) {
+  initializeApp({
+    credential: firebaseCert,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  })
+}
+
+export const db = getFirestore()
+// export const storage = getStorage().bucket()
