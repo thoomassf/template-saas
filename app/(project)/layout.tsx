95a94e5998d51c642a83eabb87f79a61a28a203b
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-04-17 20:44:29 -0300

    initial setup

--- app/(project)/layout.tsx
+++ app/(project)/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -23,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} bg-gray-100 antialiased`}
       >
         {children}
       </body>
---
+++ app/(project)/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Lading Page</h1>
+
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+}
